8b0644e88b310eb27479e6f9cac9a7a86b9d17f0
Admin <Admin@DESKTOP-55QB5I8>
2023-05-25 11:57:18 +0530

    First Commit

---
+++ src/Basic/Test1.java
@@ -0,0 +1,20 @@
+package Basic;
+
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println("Main method is running");
+		
+		m1();
+	}
+	
+	public static void m1()
+	{
+		
+		System.out.println("Hi");
+	}
+	
+}
